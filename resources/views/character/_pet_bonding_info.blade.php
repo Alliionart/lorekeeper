eded73c84458735525ee09a01316eb3d483be52d
Alliz <58315340+Alliionart@users.noreply.github.com>
2025-09-05 18:23:48 -0600

    Remove ability to bond
    Remove the ability for players to bond with pets (but still have the progress bar)

--- resources/views/character/_pet_bonding_info.blade.php
+++ resources/views/character/_pet_bonding_info.blade.php
@@ -9,15 +9,14 @@
                     {!! $pet->pet_name !!}
                 </span> the
             @endif
-            {!! $pet->pet->displayName !!}
+            {!! $pet->pet->displayName !!} ({{ $pet->level?->levelName }})
             <div class="progress mb-2">
                 <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar"
-                    style="width: {{ ($pet->level?->nextLevel?->bonding_required ? $pet->level?->bonding / $pet->level?->nextLevel?->bonding_required : 1 * 100) . '%' }}" aria-valuenow="{{ $pet->level?->bonding }}" aria-valuemin="0"
+                    style="width: {{ ($pet->level?->nextLevel?->bonding_required ? ($pet->level?->bonding / $pet->level?->nextLevel?->bonding_required) * 100 : 100) . '%' }}" aria-valuenow="{{ $pet->level?->bonding }}" aria-valuemin="0"
                     aria-valuemax="{{ $pet->level?->nextLevel?->bonding_required ?? 100 }}">
                     {{ $pet->level?->nextLevel?->bonding_required ? $pet->level?->bonding . '/' . $pet->level?->nextLevel?->bonding_required : 'Max' }}
                 </div>
             </div>
-            {{ $pet->level?->levelName }}
         </div>
     </div>
 </div>
