96640d94b5e1efeef848adb9a842365a897c7166
Alliz <58315340+Alliionart@users.noreply.github.com>
2025-09-20 22:08:36 -0600

    Add lazy loading to images because there are a ton of images

--- resources/views/designhub/_entry.blade.php
+++ resources/views/designhub/_entry.blade.php
@@ -2,7 +2,7 @@
     <div class="card-body">
         <div class="row">
             <div class="col-md-4">
-                <a href="{{ $url }}"><img src="{{ $imageUrl }}" alt="" class="img-fluid" /></a>
+                <a href="{{ $url }}"><img src="{{ $imageUrl }}" loading="lazy" alt="" class="img-fluid" /></a>
             </div>
             <div class="col-md-8">
                 <h4><a href="{{ $url }}">{{ $name }}</a></h4>
