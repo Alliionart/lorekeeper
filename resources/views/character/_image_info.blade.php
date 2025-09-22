89abf9c9987e2d02425c7fd11a818f949647a755
Alliionart <Alliionart@users.noreply.github.com>
2025-09-22 22:34:50 +0000

    refactor: fix blade formatting

--- resources/views/character/_image_info.blade.php
+++ resources/views/character/_image_info.blade.php
@@ -59,7 +59,9 @@
                             <a href="{{ $image->transformation->url }}">
                                 {!! $image->transformation->displayName !!}
                             </a>
-                            @if($image->transformation_description) ({{ $image->transformation_description }}) @endif
+                            @if ($image->transformation_description)
+                                ({{ $image->transformation_description }})
+                            @endif
                         </div>
                     </div>
                 @endif
@@ -255,4 +257,3 @@
     </div>
 
 </div>
-
