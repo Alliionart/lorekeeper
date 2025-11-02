e84c375070912f25364f5759da979110804406a2
Alliionart <Alliionart@users.noreply.github.com>
2025-11-02 01:34:33 +0000

    refactor: fix blade formatting

--- resources/views/dailies/_loot_select.blade.php
+++ resources/views/dailies/_loot_select.blade.php
@@ -12,13 +12,16 @@
         </tr>
     </thead>
     <tbody id="lootTableBody">
-        @if($loots)
-            @foreach($loots as $loot)
+        @if ($loots)
+            @foreach ($loots as $loot)
                 <tr class="loot-row">
                     <td>{!! Form::text('step[]', $loot->step, ['class' => 'form-control bg-dark text-light']) !!}</td>
-                    <td>{!! Form::select('rewardable_type[]', ['Item' => 'Item', 'Currency' => 'Currency'] + ($showLootTables ? ['LootTable' => 'Loot Table'] : []) + ($showRaffles ? ['Raffle' => 'Raffle Ticket'] : []), $loot->rewardable_type, ['class' => 'form-control reward-type', 'placeholder' => 'Select Reward Type']) !!}</td>
+                    <td>{!! Form::select('rewardable_type[]', ['Item' => 'Item', 'Currency' => 'Currency'] + ($showLootTables ? ['LootTable' => 'Loot Table'] : []) + ($showRaffles ? ['Raffle' => 'Raffle Ticket'] : []), $loot->rewardable_type, [
+                        'class' => 'form-control reward-type',
+                        'placeholder' => 'Select Reward Type',
+                    ]) !!}</td>
                     <td class="loot-row-select">
-                        @if($loot->rewardable_type == 'Item')
+                        @if ($loot->rewardable_type == 'Item')
                             {!! Form::select('rewardable_id[]', $items, $loot->rewardable_id, ['class' => 'form-control item-select selectize', 'placeholder' => 'Select Item']) !!}
                         @elseif($loot->rewardable_type == 'Currency')
                             {!! Form::select('rewardable_id[]', $currencies, $loot->rewardable_id, ['class' => 'form-control currency-select selectize', 'placeholder' => 'Select Currency']) !!}
