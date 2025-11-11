36dd319e4fe087b386edc3d2c86974526be83227
Alliz <58315340+Alliionart@users.noreply.github.com>
2025-11-11 16:55:00 -0700

    Swap to using config
    Mercury and newt suggested changing these features to use the config instead.

---
+++ config/lorekeeper/guilds.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Guilds Config
+    |--------------------------------------------------------------------------
+    |
+    | This is a list of settings for guilds.
+    | Anytime you update these settings please clear the cache!
+    |
+    */
+
+    'enable_inventories'  => 1, //Enables guilds to have an inventory!
+
+    'enable_banks'  => 1, //Enables guilds to have a bank!
+
+    'enable_shops'  => 1, //Enables guilds to have a shop!
+
+    'enable_daycare'  => 0, //Enables guilds to have a pet daycare! NOTE: Please only use if you have newt's Pet extension active, otherwise this will cause errors!
+
+    'enable_armory'  => 0, //Enables guilds to have an armor! NOTE: Please only use if you have newt's Claymores & Companions extension active, otherwise this will cause errors!
+
+];
