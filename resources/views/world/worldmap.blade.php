bb268f78d2d60c648e2d52324e405bd0fe1232eb
Alliionart <Alliionart@users.noreply.github.com>
2026-01-06 02:06:01 +0000

    refactor: fix blade formatting

--- resources/views/world/worldmap.blade.php
+++ resources/views/world/worldmap.blade.php
@@ -9,8 +9,6 @@
     <h1>World Map</h1>
 
     <div id="map"></div>
-
-    
 @endsection
 @section('scripts')
     <script>
@@ -20,26 +18,24 @@
     </script>
 @endsection
 @push('head_scripts_end')
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-        integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
-        crossorigin=""/>
-    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-     integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
-     crossorigin=""></script>
-     <style>
-        #map { height: 50vh; }
-     </style>
-     <script>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+    <style>
+        #map {
+            height: 50vh;
+        }
+    </style>
+    <script>
         $(document).ready(function() {
             var map = L.map('map').setView([51.505, -0.09], 13);
 
             //https://commenthol.github.io/leaflet-rastercoords/
             //https://gdal.org/en/stable/programs/gdal2tiles.html
-        
+
             L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 maxZoom: 19,
                 attribution: '&copy; <a href="/credits">{{ config('lorekeeper.settings.site_name', 'Lorekeeper') }}</a>'
             }).addTo(map);
         })
-     </script>
+    </script>
 @endpush
