362a736295ae508b73bf2262614474149fa67e5b
Alliionart <Alliionart@users.noreply.github.com>
2026-09-11 04:41:16 +0000

    refactor: fix blade formatting

--- resources/views/guilds/guild_settings.blade.php
+++ resources/views/guilds/guild_settings.blade.php
@@ -16,7 +16,7 @@
             <h1>Edit {{ $guild->name }}</h1>
             <p>Edit your {{ __('guilds.guild') }} below. Only {{ __('guilds.guild') }} owners and mods may edit the guild. Staff may edit your guild as well.</p>
         </div>
-        @if ( Auth::check() && (Auth::user()->id == $guild->owner_id || Auth::user()->isStaff) )
+        @if (Auth::check() && (Auth::user()->id == $guild->owner_id || Auth::user()->isStaff))
             <div class="col-md-2 text-right">
                 <a class="btn btn-danger disband">Disband Guild</a>
             </div>
@@ -171,23 +171,23 @@
     </div>
 
     <div class="modal fade" id="disbandModal" tabindex="-1" role="dialog">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <span class="modal-title h5 mb-0">Confirm Disbanding</span>
-                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <span class="modal-title h5 mb-0">Confirm Disbanding</span>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body">
+                    {!! Form::open(['url' => '/guilds/' . $guild->id . '/disband']) !!}
+                    <p>This will disband the {{ __('guilds.guild') }} and remove all members and characters. This action cannot be undone.</p>
+                    <div class="text-right">
+                        {!! Form::submit('Disband Guild', ['class' => 'btn btn-danger', 'id' => 'disbandSubmit']) !!}
                     </div>
-                    <div class="modal-body">
-                        {!! Form::open(['url' => '/guilds/' . $guild->id . '/disband']) !!}
-                        <p>This will disband the {{ __('guilds.guild') }} and remove all members and characters. This action cannot be undone.</p>
-                        <div class="text-right">
-                            {!! Form::submit('Disband Guild', ['class' => 'btn btn-danger', 'id' => 'disbandSubmit']) !!}
-                        </div>
-                        {!! Form::close() !!}
-                    </div>
+                    {!! Form::close() !!}
                 </div>
             </div>
         </div>
+    </div>
 @endsection
 
 @section('scripts')
